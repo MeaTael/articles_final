7672a53d55223e5c26befa22611cd7c69f4c8892
Max Didur <msdidur@edu.hse.ru>
2024-05-20 14:52:00 +0300

    Added confs.js

---
+++ client/src/confs.js
@@ -0,0 +1,494 @@
+export const confs = [
+    {
+        value: "AAAI",
+        label: "AAAI"
+    },
+    {
+        value: "ACL",
+        label: "ACL"
+    },
+    {
+        value: "ACM",
+        label: "ACM"
+    },
+    {
+        value: "ACT",
+        label: "ACT"
+    },
+    {
+        value: "AIAA",
+        label: "AIAA"
+    },
+    {
+        value: "AIIDE",
+        label: "AIIDE"
+    },
+    {
+        value: "AIRC",
+        label: "AIRC"
+    },
+    {
+        value: "AISTATS",
+        label: "AISTATS"
+    },
+    {
+        value: "AMIA",
+        label: "AMIA"
+    },
+    {
+        value: "ANNPR",
+        label: "ANNPR"
+    },
+    {
+        value: "AREA",
+        label: "AREA"
+    },
+    {
+        value: "ASCC",
+        label: "ASCC"
+    },
+    {
+        value: "BDL",
+        label: "BDL"
+    },
+    {
+        value: "CAIN",
+        label: "CAIN"
+    },
+    {
+        value: "CHI",
+        label: "CHI"
+    },
+    {
+        value: "CSE",
+        label: "CSE"
+    },
+    {
+        value: "CSSL",
+        label: "CSSL"
+    },
+    {
+        value: "CUDA",
+        label: "CUDA"
+    },
+    {
+        value: "CVPR",
+        label: "CVPR"
+    },
+    {
+        value: "DAC",
+        label: "DAC"
+    },
+    {
+        value: "DAS",
+        label: "DAS"
+    },
+    {
+        value: "DCASE",
+        label: "DCASE"
+    },
+    {
+        value: "DOLAP",
+        label: "DOLAP"
+    },
+    {
+        value: "DSAA",
+        label: "DSAA"
+    },
+    {
+        value: "EACL",
+        label: "EACL"
+    },
+    {
+        value: "ECML",
+        label: "ECML"
+    },
+    {
+        value: "ECOC",
+        label: "ECOC"
+    },
+    {
+        value: "ECSA",
+        label: "ECSA"
+    },
+    {
+        value: "ELM",
+        label: "ELM"
+    },
+    {
+        value: "EMNLP",
+        label: "EMNLP"
+    },
+    {
+        value: "EMS",
+        label: "EMS"
+    },
+    {
+        value: "ESANN",
+        label: "ESANN"
+    },
+    {
+        value: "FAIM",
+        label: "FAIM"
+    },
+    {
+        value: "FMAS",
+        label: "FMAS"
+    },
+    {
+        value: "FTC",
+        label: "FTC"
+    },
+    {
+        value: "GECCO",
+        label: "GECCO"
+    },
+    {
+        value: "GHC",
+        label: "GHC"
+    },
+    {
+        value: "GPTP",
+        label: "GPTP"
+    },
+    {
+        value: "GRL",
+        label: "GRL"
+    },
+    {
+        value: "HCAI",
+        label: "HCAI"
+    },
+    {
+        value: "HCI",
+        label: "HCI"
+    },
+    {
+        value: "HCOMP",
+        label: "HCOMP"
+    },
+    {
+        value: "HLT",
+        label: "HLT"
+    },
+    {
+        value: "HRI",
+        label: "HRI"
+    },
+    {
+        value: "ICAART",
+        label: "ICAART"
+    },
+    {
+        value: "ICAIF",
+        label: "ICAIF"
+    },
+    {
+        value: "ICANN",
+        label: "ICANN"
+    },
+    {
+        value: "ICARCV",
+        label: "ICARCV"
+    },
+    {
+        value: "ICASSP",
+        label: "ICASSP"
+    },
+    {
+        value: "ICDE",
+        label: "ICDE"
+    },
+    {
+        value: "ICLR",
+        label: "ICLR"
+    },
+    {
+        value: "ICML",
+        label: "ICML"
+    },
+    {
+        value: "ICMLA",
+        label: "ICMLA"
+    },
+    {
+        value: "ICRA",
+        label: "ICRA"
+    },
+    {
+        value: "ICTAI",
+        label: "ICTAI"
+    },
+    {
+        value: "IEEE",
+        label: "IEEE"
+    },
+    {
+        value: "IFAC",
+        label: "IFAC"
+    },
+    {
+        value: "IJCAI",
+        label: "IJCAI"
+    },
+    {
+        value: "IJCNN",
+        label: "IJCNN"
+    },
+    {
+        value: "INDICON",
+        label: "INDICON"
+    },
+    {
+        value: "INTERSPEECH",
+        label: "INTERSPEECH"
+    },
+    {
+        value: "IOP",
+        label: "IOP"
+    },
+    {
+        value: "IPDPS",
+        label: "IPDPS"
+    },
+    {
+        value: "IPSI",
+        label: "IPSI"
+    },
+    {
+        value: "ISBI",
+        label: "ISBI"
+    },
+    {
+        value: "ISBN",
+        label: "ISBN"
+    },
+    {
+        value: "ISSCC",
+        label: "ISSCC"
+    },
+    {
+        value: "ISSN",
+        label: "ISSN"
+    },
+    {
+        value: "IST",
+        label: "IST"
+    },
+    {
+        value: "ITW",
+        label: "ITW"
+    },
+    {
+        value: "JAIR",
+        label: "JAIR"
+    },
+    {
+        value: "JMLR",
+        label: "JMLR"
+    },
+    {
+        value: "JPSJ",
+        label: "JPSJ"
+    },
+    {
+        value: "KAIS",
+        label: "KAIS"
+    },
+    {
+        value: "KDD",
+        label: "KDD"
+    },
+    {
+        value: "LXAI",
+        label: "LXAI"
+    },
+    {
+        value: "MEXT",
+        label: "MEXT"
+    },
+    {
+        value: "MIC",
+        label: "MIC"
+    },
+    {
+        value: "MICCAI",
+        label: "MICCAI"
+    },
+    {
+        value: "MIR",
+        label: "MIR"
+    },
+    {
+        value: "MIT",
+        label: "MIT"
+    },
+    {
+        value: "MLJ",
+        label: "MLJ"
+    },
+    {
+        value: "MLOSS",
+        label: "MLOSS"
+    },
+    {
+        value: "MLPH",
+        label: "MLPH"
+    },
+    {
+        value: "MLSP",
+        label: "MLSP"
+    },
+    {
+        value: "MRCL",
+        label: "MRCL"
+    },
+    {
+        value: "NCVX",
+        label: "NCVX"
+    },
+    {
+        value: "NIPS",
+        label: "NIPS"
+    },
+    {
+        value: "NSF",
+        label: "NSF"
+    },
+    {
+        value: "OAGM",
+        label: "OAGM"
+    },
+    {
+        value: "PAKDD",
+        label: "PAKDD"
+    },
+    {
+        value: "PHM",
+        label: "PHM"
+    },
+    {
+        value: "PMAPS",
+        label: "PMAPS"
+    },
+    {
+        value: "PMLR",
+        label: "PMLR"
+    },
+    {
+        value: "PODS",
+        label: "PODS"
+    },
+    {
+        value: "PPML",
+        label: "PPML"
+    },
+    {
+        value: "PSI",
+        label: "PSI"
+    },
+    {
+        value: "RAMS",
+        label: "RAMS"
+    },
+    {
+        value: "RRPR",
+        label: "RRPR"
+    },
+    {
+        value: "SAI",
+        label: "SAI"
+    },
+    {
+        value: "SBAI",
+        label: "SBAI"
+    },
+    {
+        value: "SIGACT",
+        label: "SIGACT"
+    },
+    {
+        value: "SIGKDD",
+        label: "SIGKDD"
+    },
+    {
+        value: "SIGMOD",
+        label: "SIGMOD"
+    },
+    {
+        value: "SIGSPATIAL",
+        label: "SIGSPATIAL"
+    },
+    {
+        value: "SIPAIM",
+        label: "SIPAIM"
+    },
+    {
+        value: "SN",
+        label: "SN"
+    },
+    {
+        value: "SOUPS",
+        label: "SOUPS"
+    },
+    {
+        value: "SPIE",
+        label: "SPIE"
+    },
+    {
+        value: "SPLASH",
+        label: "SPLASH"
+    },
+    {
+        value: "STOC",
+        label: "STOC"
+    },
+    {
+        value: "TAILOR",
+        label: "TAILOR"
+    },
+    {
+        value: "TCAD",
+        label: "TCAD"
+    },
+    {
+        value: "TMCE",
+        label: "TMCE"
+    },
+    {
+        value: "TPAMI",
+        label: "TPAMI"
+    },
+    {
+        value: "TPS",
+        label: "TPS"
+    },
+    {
+        value: "TQC",
+        label: "TQC"
+    },
+    {
+        value: "UAI",
+        label: "UAI"
+    },
+    {
+        value: "UCSD",
+        label: "UCSD"
+    },
+    {
+        value: "USENIX",
+        label: "USENIX"
+    },
+    {
+        value: "VTC",
+        label: "VTC"
+    },
+    {
+        value: "WSDM",
+        label: "WSDM"
+    },
+    {
+        value: "ZTE",
+        label: "ZTE"
+    }
+]
